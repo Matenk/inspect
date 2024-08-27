a4f407fdf88fba47014463fdc6cc10dd781c2e98
Matenk <167009609+Matenk@users.noreply.github.com>
2024-08-28 00:05:35 +0300

    Update Inspect.py

--- Inspect.py
+++ Inspect.py
@@ -3,23 +3,37 @@ import types
 
 
 def introspection_info(obj):
-    
-    obj_type = type(obj).__name__
+    obj_type = type(obj)
 
     attributes = dir(obj)
+    attrib = []
+    method = []
+    for attr in attributes:
+        if callable(getattr(obj, attr)):
+            method.append(attr)
+        else:
+            attrib.append(attr)
 
-    methods = [method for method in attributes if callable(getattr(obj, method))]
+
+
+
+
 
     module = inspect.getmodule(obj)
 
     info = {
         'type': obj_type,
-        'attributes': attributes,
-        'methods': methods,
-        'module': module
+        'attributes': attrib,
+        'methods': method,
+        'module': module,
+
     }
 
-    
     return info
+
+
+print(introspection_info(5))
+
+
 
 
